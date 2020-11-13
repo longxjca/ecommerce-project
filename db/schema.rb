cd58b9a0a840e4ecebbd022f4e0cb1cc9195c1f1
Longxun Jin <longxjca@gmail.com>
2020-11-13 02:54:57 -0600

    migrate genre table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_085133) do
+ActiveRecord::Schema.define(version: 2020_11_13_085422) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2020_11_13_085133) do
   end
 
   create_table "developers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
