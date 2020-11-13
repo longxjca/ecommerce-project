cf2fb31eea6905e589855c995b0549a1230303b6
Longxun Jin <longxjca@gmail.com>
2020-11-13 17:15:21 -0600

    migrate product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_111501) do
+ActiveRecord::Schema.define(version: 2020_11_13_231410) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "user_name"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 2020_11_13_111501) do
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["developer_id"], name: "index_products_on_developer_id"
     t.index ["publisher_id"], name: "index_products_on_publisher_id"
   end
