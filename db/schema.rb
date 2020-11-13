d6d72f124e06cc2a03c513b710bbe1a58ebc628c
Longxun Jin <longxjca@gmail.com>
2020-11-13 05:15:46 -0600

    migrate page table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_093446) do
+ActiveRecord::Schema.define(version: 2020_11_13_111501) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "user_name"
@@ -82,6 +82,14 @@ ActiveRecord::Schema.define(version: 2020_11_13_093446) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["account_id"], name: "index_orders_on_account_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "product_orders", force: :cascade do |t|
