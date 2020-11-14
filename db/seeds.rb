c6cbb33855a9f39dd04a9d45f11a5421d5f4bd06
Longxun Jin <longxjca@gmail.com>
2020-11-14 04:39:06 -0600

    implemented seeds.rb file to populate product table, developers table and publisher table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+GenreProduct.destroy_all
+ProductOrder.destroy_all
+Product.destroy_all
+Order.destroy_all
+Account.destroy_all
+Province.destroy_all
+Genre.destroy_all
+Developer.destroy_all
+Publisher.destroy_all
+Page.destroy_all
+AdminUser.destroy_all
+
 if Rails.env.development?
   AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
 end
+steam_csv = File.read(Rails.root.join("db/steam.csv"))
+games = CSV.parse(steam_csv, headers: true, encoding: "utf-8")
+description_csv = File.read(Rails.root.join("db/steam_description_data.csv"))
+descriptions = CSV.parse(description_csv, headers: true, encoding: "utf-8")
+media_csv = File.read(Rails.root.join("db/steam_media_data.csv"))
+images = CSV.parse(media_csv, headers: true, encoding: "utf-8")
+
+games.each do |game|
+  game_description = ""
+  descriptions.each do |description|
+    if description["steam_appid"] == game["appid"]
+      game_description = description["detailed_description"]
+      break
+    end
+  end
+
+  developer_entry = Developer.find_or_create_by(name: game["developer"])
+  publisher_entry = Publisher.find_or_create_by(name: game["publisher"])
+
+  Product.find_or_create_by(description:  game_description,
+                            name:         game["name"],
+                            price:        game["price"],
+                            developer:    developer_entry,
+                            publisher:    publisher_entry,
+                            release_date: game["release_date"])
+end
+puts "Created #{Product.count} products"
+puts "Created Joint tables rows"
