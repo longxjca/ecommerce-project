c40ed0b1114a4a030df0a42fe6cc2faccb7e6efd
Longxun Jin <longxjca@gmail.com>
2020-11-13 22:06:58 -0600

    moved admin files

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,11 +1,10 @@
 ActiveAdmin.register Product do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :description, :name, :price, :developer_id, :publisher_id, :release_date, :status
+  permit_params :description, :name, :price, :developer_id, :publisher_id, :release_date, :status
   #
   # or
   #
@@ -14,5 +13,4 @@ ActiveAdmin.register Product do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
