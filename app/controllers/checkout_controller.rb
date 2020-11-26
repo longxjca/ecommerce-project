0f08b7fbc503c1a981e6c91ae398c3dd944ff644
Longxun Jin <longxjca@gmail.com>
2020-11-25 22:40:18 -0600

    implemented stripe for single product payment

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,2 +1,42 @@
 class CheckoutController < ApplicationController
+  def create
+    product = Product.find(params[:id])
+    if product.nil?
+      redirect_to root_pah
+      return
+    end
+    # setting up a stripe session for payment
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ["card"],
+      line_items:           [{
+        name:        product.name,
+        description: product.description,
+        amount:      (product.price * 100).to_i,
+        currency:    "cad",
+        quantity:    1
+
+      },
+                             {
+                               name:        "GST",
+                               description: "Goods and Services Tax",
+                               amount:      (product.price * 0.05 * 100).to_i,
+                               currency:    "cad",
+                               quantity:    1
+
+                             }],
+      success_url:          checkout_success_url + "?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url:           checkout_cancel_url
+    )
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def cancel; end
+
+  def success
+    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope "/checkout" do
-    post "creat", to: "checkout#create", as: "checkout_create"
+    post "create", to: "checkout#create", as: "checkout_create"
     get "cancel", to: "checkout#cancel", as: "checkout_cancel"
     get "success", to: "checkout#success", as: "checkout_success"
   end
